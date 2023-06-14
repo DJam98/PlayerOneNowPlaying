008f1ce2404680930ccd79cf6c24a98841def52d
DJam98 <57510174+DJam98@users.noreply.github.com>
2023-06-14 10:20:15 +1000

    Week 7

--- info.js
+++ info.js
@@ -2,7 +2,7 @@ let song = [];
 let artist = [];
 let segment = [];
 
-// 2023 | Season 2 | Week 4
+// 2023 | Season 2 | Week 7
 
 // Segment Topics
 
@@ -10,72 +10,72 @@ segment [0] = "Starting Soon"
 segment [1] = "Welcome"
 segment [2] = "New and News"
 segment [3] = "The Playlist"
-segment [4] = "For The Glory"
+segment [4] = "Gotta Go Fast"
 segment [5] = "The Checkpoint"
-segment [6] = "Your Way Or The Highway"
-segment [7] = "What's Next"
-segment [8] = "Better E3"
+segment [6] = "Pay For Your Sins"
+segment [7] = "State of Play"
+segment [8] = "Borrowing Your Homework"
 segment [9] = "The Quiz"
 segment [10] = "Wrap Up"
 
 // Songs and Artists
 
-song [1] = "new heartbreak"
-artist [1] = "sad alex"
+song [1] = "Dreams"
+artist [1] = "Adventure Club (ft. ELEA)"
 
-song [2] = "nice to me"
-artist [2] = "piri, Tommy Villiers"
+song [2] = "condition of us"
+artist [2] = "La Force"
 
-song [3] = "All Of The Time"
-artist [3] = "Jungle"
+song [3] = "Colgera Battle"
+artist [3] = "Nintendo"
 
-song [4] = "Torn"
-artist [4] = "Jakka, Grace Menamie"
+song [4] = "eight"
+artist [4] = "iokim, ft. LORA"
 
-song [5] = "Get Down"
-artist [5] = "FJØRA"
+song [5] = "Everything Goes On"
+artist [5] = "Peter Robinson"
 
-song [6] = "Romance + Rebellion"
-artist [6] = "LÁLKA"
+song [6] = "All Like That"
+artist [6] = "moktar"
 
-song [7] = "World of Colours"
-artist [7] = "Hydelic"
+song [7] = "God Devouring Mania"
+artist [7] = "HoyoMix"
 
-song [8] = "Needles to Say"
-artist [8] = "Fascinator"
+song [8] = "Over and Over"
+artist [8] = "Becca Mancari"
 
-song [9] = "#8 regret"
-artist [9] = "Qumu"
+song [9] = "VISIONS"
+artist [9] = "eeJ, Safari Riot"
 
-song [10] = "Note to Self"
-artist [10] = "Rahill"
+song [10] = "MEDICINE"
+artist [10] = "Soma"
 
-song [11] = "Colgera Battle"
-artist [11] = "Nintendo"
+song [11] = "All of the Time"
+artist [11] = "Jungle"
 
-song [12] = "Through my Teeth"
-artist [12] = "Ukiyo, Emila Ali"
+song [12] = "Sodom & Gomorrah"
+artist [12] = "Dorian Electra"
 
-song [13] = "Goodbye"
-artist [13] = "Ramsey"
+song [13] = "Snake Eater"
+artist [13] = "Cythia Harrell"
 
-song [14] = "She's on Fire"
-artist [14] = "Strange Ranger"
+song [14] = "Snowman"
+artist [14] = "Blonde Redhead"
 
-song [15] = "Snake Eater"
-artist [15] = "Cythia Harrell"
+song [15] = "Ska-Blam!"
+artist [15] = "Yoko and the Gold Bazookas"
 
-song [16] = "Just Cool"
-artist [16] = "Obongjayar"
+song [16] = "Dedicated to Tar Feather"
+artist [16] = "McKinley Dixon, Anjimile"
 
-song [17] = "Champions"
-artist [17] = "The Tech Thieves"
+song [17] = "Free Radicals"
+artist [17] = "The Flaming Lips"
 
-song [18] = "Hello Goodbye"
-artist [18] = "Stevan"
+song [18] = "Romance + Rebellion"
+artist [18] = "LÂLKA"
 
-song [19] = "Summer Healing"
-artist [19] = "STVN, Palm City"
+song [19] = "Never A Good Time"
+artist [19] = "NOTD, The Band CAMINO"
 
 // End of Show
 
