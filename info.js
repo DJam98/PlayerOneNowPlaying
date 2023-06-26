8ec0bff136764cb7fae13066b406b1b27dd0ac30
DJam98 <57510174+DJam98@users.noreply.github.com>
2023-06-26 23:55:54 +1000

    S2 W10

--- info.js
+++ info.js
@@ -10,74 +10,77 @@ segment [0] = "Starting Soon"
 segment [1] = "Welcome"
 segment [2] = "New and News"
 segment [3] = "The Playlist"
-segment [4] = "Take A Bite Out Of That Apple"
+segment [4] = "A Local 100 Player Battle Royale?"
 segment [5] = "The Checkpoint"
-segment [6] = "Let's Play... IRL?"
-segment [7] = "Let's Play... IRL?"
-segment [8] = "Get Ready For October"
-segment [9] = "Get Ready For October"
+segment [6] = "Mario Mario Mario"
+segment [7] = "Issue #2023"
+segment [8] = "Issue #2023"
+segment [9] = "Rapid Review"
 segment [10] = "Wrap Up"
 
 // Songs and Artists
 
-song [1] = "Rules (Are Meant To Break)"
-artist [1] = "Che Lingo, League of Legends"
+song [1] = "vs. Sayu"
+artist [1] = "James Landino, Nikki Simmons"
 
-song [2] = "Space Junk"
-artist [2] = "Miso Extra"
+song [2] = "sunsets"
+artist [2] = "Kee’ahn"
 
-song [3] = "Tetris 99 Main Theme"
-artist [3] = "Teruo Taniguchi"
+song [3] = "Connected (Yours Forever)"
+artist [3] = "Hydelic"
 
-song [4] = "Get What I Want"
-artist [4] = "Loosie Grind"
+song [4] = "All The Choices"
+artist [4] = "Skating Polly"
 
-song [5] = "The Demon Dragon"
-artist [5] = "Nintendo"
+song [5] = "Playground"
+artist [5] = "Bea Miller"
 
-song [6] = "Limes"
-artist [6] = "Jasper Tygner"
+song [6] = "Motion"
+artist [6] = "King IV"
 
-song [7] = "Three-Horn Circus"
-artist [7] = "Yoko and the Gold Bazookas"
+song [7] = "You Can Get It"
+artist [7] = "Arkells, K.Flay"
 
-song [8] = "Off Wiv Ya Headz"
-artist [8] = "Nia Archives"
+song [8] = "Brat"
+artist [8] = "Dusty"
 
-song [9] = "Hummingbird"
-artist [9] = "Metro Boomin, James Blake"
+song [9] = "Hero"
+artist [9] = "Martin Garrix, JVKE"
 
-song [10] = "Maybe We’re Running"
-artist [10] = "Paulina"
+song [10] = "Feel Alive"
+artist [10] = "Cat & Calmell"
 
-song [11] = "Hello Hello Hello"
-artist [11] = "Remi Wolf"
+song [11] = "Ride or Fry"
+artist [11] = "Octoplush"
 
-song [12] = "Girls"
-artist [12] = "Theia"
+song [12] = "Fader"
+artist [12] = "Róisín Murphy, DJ Koze"
 
-song [13] = ">one (greater than one)"
-artist [13] = "VALORANT, ericdo"
+song [13] = "Die For You"
+artist [13] = "Grabbitz, VALORANT"
 
-song [14] = "I Gotta Feeling"
-artist [14] = "STONE"
+song [14] = "Highway"
+artist [14] = "Lewis OfMan, Empress Of"
 
-song [15] = "Starfield Main Theme"
-artist [15] = "London Symphony Orchestra"
+song [15] = "Não Sei Parar"
+artist [15] = "Bhaskar, Afterclapp"
 
-song [16] = " bones (sinking like stones)"
-artist [16] = "44 Ardent"
+song [16] = "Where My Asians At?!"
+artist [16] = "Reshma"
 
-song [17] = "Major Crimes"
-artist [17] = "HEALTH"
+song [17] = "Reflection - Centre of the Earth Mix"
+artist [17] = "Jukio Kallio"
 
-song [18] = "Sleeves Touch My Elbows"
-artist [18] = "1tbsp, Mora y los Metegoles"
+song [18] = "I Don’t Hear You Anymore"
+artist [18] = "The Money War"
 
-song [19] = "SLEEP PARTY"
-artist [19] = "Tokimeki Records, Desired (ft. mindfreakkk)"
+song [19] = "Back One Day (Outro Song)"
+artist [19] = "TheFatRat, NEFFEX"
 
 // End of Show
 
-song [20] = "See Us Again Next Week"
-artist [20] = "Mondays 8pm AEST"
+// song [20] = "See Us Again Next Week"
+// artist [20] = "Mondays 8pm AEST"
+
+song [20] = "We'll Be Back For Season Three"
+artist [20] = "July 10th"
