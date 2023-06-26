ce9260fc26183b96a3017f2a5fd1b2e5f485892c
DJam98 <57510174+DJam98@users.noreply.github.com>
2023-06-26 16:36:57 +1000

    s2w9

--- info.js
+++ info.js
@@ -10,72 +10,72 @@ segment [0] = "Starting Soon"
 segment [1] = "Welcome"
 segment [2] = "New and News"
 segment [3] = "The Playlist"
-segment [4] = "Gotta Go Fast"
+segment [4] = "Take A Bite Out Of That Apple"
 segment [5] = "The Checkpoint"
-segment [6] = "Pay For Your Sins"
-segment [7] = "State of Play"
-segment [8] = "Borrowing Your Homework"
-segment [9] = "The Quiz"
+segment [6] = "Let's Play... IRL?"
+segment [7] = "Let's Play... IRL?"
+segment [8] = "Get Ready For October"
+segment [9] = "Get Ready For October"
 segment [10] = "Wrap Up"
 
 // Songs and Artists
 
-song [1] = "Dreams"
-artist [1] = "Adventure Club (ft. ELEA)"
+song [1] = "Rules (Are Meant To Break)"
+artist [1] = "Che Lingo, League of Legends"
 
-song [2] = "condition of us"
-artist [2] = "La Force"
+song [2] = "Space Junk"
+artist [2] = "Miso Extra"
 
-song [3] = "Colgera Battle"
-artist [3] = "Nintendo"
+song [3] = "Tetris 99 Main Theme"
+artist [3] = "Teruo Taniguchi"
 
-song [4] = "eight"
-artist [4] = "iokim, ft. LORA"
+song [4] = "Get What I Want"
+artist [4] = "Loosie Grind"
 
-song [5] = "Everything Goes On"
-artist [5] = "Peter Robinson"
+song [5] = "The Demon Dragon"
+artist [5] = "Nintendo"
 
-song [6] = "All Like That"
-artist [6] = "moktar"
+song [6] = "Limes"
+artist [6] = "Jasper Tygner"
 
-song [7] = "God Devouring Mania"
-artist [7] = "HoyoMix"
+song [7] = "Three-Horn Circus"
+artist [7] = "Yoko and the Gold Bazookas"
 
-song [8] = "Over and Over"
-artist [8] = "Becca Mancari"
+song [8] = "Off Wiv Ya Headz"
+artist [8] = "Nia Archives"
 
-song [9] = "VISIONS"
-artist [9] = "eeJ, Safari Riot"
+song [9] = "Hummingbird"
+artist [9] = "Metro Boomin, James Blake"
 
-song [10] = "MEDICINE"
-artist [10] = "Soma"
+song [10] = "Maybe We’re Running"
+artist [10] = "Paulina"
 
-song [11] = "All of the Time"
-artist [11] = "Jungle"
+song [11] = "Hello Hello Hello"
+artist [11] = "Remi Wolf"
 
-song [12] = "Sodom & Gomorrah"
-artist [12] = "Dorian Electra"
+song [12] = "Girls"
+artist [12] = "Theia"
 
-song [13] = "Snake Eater"
-artist [13] = "Cythia Harrell"
+song [13] = ">one (greater than one)"
+artist [13] = "VALORANT, ericdo"
 
-song [14] = "Snowman"
-artist [14] = "Blonde Redhead"
+song [14] = "I Gotta Feeling"
+artist [14] = "STONE"
 
-song [15] = "Ska-Blam!"
-artist [15] = "Yoko and the Gold Bazookas"
+song [15] = "Starfield Main Theme"
+artist [15] = "London Symphony Orchestra"
 
-song [16] = "Dedicated to Tar Feather"
-artist [16] = "McKinley Dixon, Anjimile"
+song [16] = " bones (sinking like stones)"
+artist [16] = "44 Ardent"
 
-song [17] = "Free Radicals"
-artist [17] = "The Flaming Lips"
+song [17] = "Major Crimes"
+artist [17] = "HEALTH"
 
-song [18] = "Romance + Rebellion"
-artist [18] = "LÂLKA"
+song [18] = "Sleeves Touch My Elbows"
+artist [18] = "1tbsp, Mora y los Metegoles"
 
-song [19] = "Never A Good Time"
-artist [19] = "NOTD, The Band CAMINO"
+song [19] = "SLEEP PARTY"
+artist [19] = "Tokimeki Records, Desired (ft. mindfreakkk)"
 
 // End of Show
 
