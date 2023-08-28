4fb87c4b92353c7799552d8844095e1585a5cf70
DJam98 <57510174+DJam98@users.noreply.github.com>
2023-08-28 18:38:25 +1000

    Week 8 Season 3

--- info.js
+++ info.js
@@ -2,7 +2,7 @@ let song = [];
 let artist = [];
 let segment = [];
 
-// 2023 | Season 3 | Week 4
+// 2023 | Season 3 | Week 8
 
 // Segment Topics
 
@@ -10,76 +10,76 @@ segment [0] = "Starting Soon"
 segment [1] = "Welcome"
 segment [2] = "New and News"
 segment [3] = "The Playlist"
-segment [4] = "LIVE Review"
+segment [4] = "Mario’s Last Adventure"
 segment [5] = "The Checkpoint"
-segment [6] = "Hearing in Full Colour"
-segment [7] = "The Skill Gap"
-segment [8] = "Gold Has Faded"
-segment [9] = "The Quiz"
+segment [6] = "Worlds, Worlds, Worlds"
+segment [7] = "Jump In The Portal"
+segment [8] = "A World of Water"
+segment [9] = "Gamescon!"
 segment [10] = "Wrap Up"
 
 // Songs and Artists
 
-song [1] = "Joypunks"
-artist [1] = "Big Wild"
+song [1] = "Phoenix"
+artist [1] = "Cailin Russo, Chrissy Constanza"
 
-song [2] = "rats in my walls"
-artist [2] = "ratbag"
+song [2] = "Over and Over"
+artist [2] = "Becca Mancari"
 
-song [3] = "Tentacle To The Metal"
-artist [3] = "Damp Socks, Off the Hook"
+song [3] = "Weeping Dawn"
+artist [3] = "Barislav Slavov"
 
-song [4] = "Homesick Honeybee"
-artist [4] = "Becca Mancari"
+song [4] = "High School Drama"
+artist [4] = "Lola Scott"
 
-song [5] = "Next Chapter"
-artist [5] = "Hydelic"
+song [5] = "Mona Lisa"
+artist [5] = "Dominic Fike"
 
-song [6] = "Pieta"
-artist [6] = "Locate S,1"
+song [6] = "Dancing Mood"
+artist [6] = "Tjaka"
 
-song [7] = "Kass’ Theme"
-artist [7] = "Coffee Date"
+song [7] = "Peaches"
+artist [7] = "Jack Blac"
 
-song [8] = "TOMMY"
-artist [8] = "WALKER."
+song [8] = "Way Out"
+artist [8] = "Ta-ku, Milan Ring, Matt McWaters & Questlove"
 
-song [9] = "Bruyères"
-artist [9] = "Dan Golding"
+song [9] = "Ticking Away"
+artist [9] = "Grabbitz, bbno$"
 
-song [10] = "Nausea"
-artist [10] = "The Cherubs"
+song [10] = "Familiar Flavours"
+artist [10] = "700 Feel"
 
-song [11] = "Jump Up, Super Star!"
-artist [11] = "Kate Higgens"
+song [11] = "Let Em Pray"
+artist [11] = "NF"
 
-song [12] = "Cheerleader"
-artist [12] = "Ashnikko"
+song [12] = "SHYPOP"
+artist [12] = "Ninajirachi, ISOxo"
 
-song [13] = "Terrified"
-artist [13] = "Despotem, Faithroze"
+song [13] = "Night City"
+artist [13] = "R E L, Artemis Delta"
 
-song [14] = "Would You Be Mine"
-artist [14] = "MOZË"
+song [14] = "Dollhouse"
+artist [14] = "Theia"
 
-song [15] = "Fire To The Fuse"
-artist [15] = "Jackson Wang, 88rising"
+song [15] = "Rondeau des fleurs et des rapieres"
+artist [15] = "London Symphonic Orchestra"
 
-song [16] = "Good Girl"
-artist [16] = " June Jones"
+song [16] = "How It Feels"
+artist [16] = "Barry Can’t Swim"
 
-song [17] = "Polumnia Omnia"
-artist [17] = "HOYO-MIX"
+song [17] = "Wind Shear"
+artist [17] = "Piece Fulton"
 
-song [18] = "Use It Or Lose It"
-artist [18] = "Lynks"
+song [18] = "kamakarmic"
+artist [18] = "Holliday Howe"
 
-song [19] = "To The Limit"
-artist [19] = "City Wolf, OTTO BLUE"
+song [19] = "Fire Alarm"
+artist [19] = "Castlecomer"
 
 // End of Show
 
-song [20] = "See Us Again Next Week"
+song [20] = "We'll be back..."
 artist [20] = "Mondays 8pm AEST"
 
 // song [20] = "We'll Be Back For Season Three"
