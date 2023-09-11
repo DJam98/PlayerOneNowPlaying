f92f77c4eabff6374fd208c232a734041542112f
DJam98 <57510174+DJam98@users.noreply.github.com>
2023-09-11 19:20:26 +1000

    Update info.js

--- info.js
+++ info.js
@@ -2,7 +2,7 @@ let song = [];
 let artist = [];
 let segment = [];
 
-// 2023 | Season 3 | Week 8
+// 2023 | Season 3 | Week 10
 
 // Segment Topics
 
@@ -10,77 +10,77 @@ segment [0] = "Starting Soon"
 segment [1] = "Welcome"
 segment [2] = "New and News"
 segment [3] = "The Playlist"
-segment [4] = "Mario’s Last Adventure"
+segment [4] = "Even I Don't Know What It Does"
 segment [5] = "The Checkpoint"
-segment [6] = "Worlds, Worlds, Worlds"
-segment [7] = "Jump In The Portal"
-segment [8] = "A World of Water"
-segment [9] = "Gamescon!"
+segment [6] = "Cat's 2"
+segment [7] = "I Know What I Do Now"
+segment [8] = "Origin Story"
+segment [9] = "It's My Space!"
 segment [10] = "Wrap Up"
 
 // Songs and Artists
 
-song [1] = "Phoenix"
-artist [1] = "Cailin Russo, Chrissy Constanza"
+song [1] = "High Enough"
+artist [1] = "Grant, Will Jay"
 
-song [2] = "Over and Over"
-artist [2] = "Becca Mancari"
+song [2] = "She’s On My Mind"
+artist [2] = "Romy"
 
-song [3] = "Weeping Dawn"
-artist [3] = "Barislav Slavov"
+song [3] = "Ripstop & Go"
+artist [3] = "H2Whoa"
 
-song [4] = "High School Drama"
-artist [4] = "Lola Scott"
+song [4] = "Rehab"
+artist [4] = "d4vd"
 
-song [5] = "Mona Lisa"
-artist [5] = "Dominic Fike"
+song [5] = "Shrine Battle Remix"
+artist [5] = "David Bru"
 
-song [6] = "Dancing Mood"
-artist [6] = "Tjaka"
+song [6] = "Liquid Pearl"
+artist [6] = "Delilah Holliday"
 
-song [7] = "Peaches"
-artist [7] = "Jack Blac"
+song [7] = "Always Been but Never Dreamed"
+artist [7] = "Hydelic"
 
-song [8] = "Way Out"
-artist [8] = "Ta-ku, Milan Ring, Matt McWaters & Questlove"
+song [8] = "No Peace"
+artist [8] = "Jada Weazel"
 
-song [9] = "Ticking Away"
-artist [9] = "Grabbitz, bbno$"
+song [9] = "Reflection (Center of the Earth Mix)"
+artist [9] = "Jukio Kallio"
 
-song [10] = "Familiar Flavours"
-artist [10] = "700 Feel"
+song [10] = "inferno"
+artist [10] = "yeule"
 
-song [11] = "Let Em Pray"
-artist [11] = "NF"
+song [11] = "Built For This"
+artist [11] = "Black Prez, ft. KID SOMETHING"
 
-song [12] = "SHYPOP"
-artist [12] = "Ninajirachi, ISOxo"
+song [12] = "over my ex"
+artist [12] = "Amelia Moore"
 
-song [13] = "Night City"
-artist [13] = "R E L, Artemis Delta"
+song [13] = "Gusty Garden Galaxy"
+artist [13] = "Qumu"
 
-song [14] = "Dollhouse"
-artist [14] = "Theia"
+song [14] = "mood swings"
+artist [14] = "Chuey"
 
-song [15] = "Rondeau des fleurs et des rapieres"
-artist [15] = "London Symphonic Orchestra"
+song [15] = "Savior"
+artist [15] = "One True God, VALORANT"
 
-song [16] = "How It Feels"
-artist [16] = "Barry Can’t Swim"
+song [16] = "BITE BACK"
+artist [16] = "daine"
 
-song [17] = "Wind Shear"
-artist [17] = "Piece Fulton"
+song [17] = "The Final Feast"
+artist [17] = "HOYO-MiX"
 
-song [18] = "kamakarmic"
-artist [18] = "Holliday Howe"
+song [18] = "New Casino"
+artist [18] = "flamingo blonde"
 
-song [19] = "Fire Alarm"
-artist [19] = "Castlecomer"
+song [19] = "Could I Be"
+artist [19] = "Avenza"
 
 // End of Show
 
-song [20] = "We'll be back..."
-artist [20] = "Mondays 8pm AEST"
+// song [20] = "We'll be back..."
+// artist [20] = "Mondays 8pm AEST"
 
-// song [20] = "We'll Be Back For Season Three"
-// artist [20] = "July 10th"
+song [20] = "We'll Be Back For Season Four"
+artist [20] = "September 25th"
